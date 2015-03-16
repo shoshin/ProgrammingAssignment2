c93acb538f861285472f6c68309195e6cac2046a
shoshin <allan.due@gmail.com>
2015-03-16 14:57:40 -0600

    Assignment 2
    Assignment 2 for Coursera course R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -12,12 +12,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Ok, now we set four methods.  Functions which allow us to manipulate 
 ## the object created when we call makeMatrix on a Matrix.
+
 ## set allows us to give a new matrix to the object.  We can overwrite 
 ## the orginal matrix that was used when makeMatrix was first called
+
 ## get returns the current matrix
+
 ## setinverse lets us assign an inverse matrix to the Object rather than
-## have it calculate getinverse allows us to call the current inverse
+## have it calculate 
+
+## getinverse allows us to call the current inverse
 ## in the object.
+
 ## if we have a matrix m1 and call makeMatrix we create a new object m1_cache 
 ## by >m1_cache <- makeMatrix(m1)
 ## we can then call any of these functions by using m1_cashe$getinverse()
@@ -51,7 +57,7 @@ makeCacheMatrix <- function(x = matrix()) {
         
         getinverse <- function() inver
 
-## This is a list used by the function to tell what is available in the object
+## This is a list used by the function to tell what is available in the list object
         
         list(set = set, get = get,
              setinverse = setinverse,
@@ -60,19 +66,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## We use this function to actually get the inverse of the matrix for the
+## We use cacheSolve to actually get the inverse of the matrix for the
 ## object the first time
-## I honestly don't like this.  I if I was making an object that controlled 
-## the inverse of a matrix I would make the call to the function solve in 
-## the object. I.E In the makeCache I would check and see if the inverse was 
-## set when getinverse was called.  
-## If it hadn't been set I would make the call to the function solve then. 
-## If it had been set I would return it.  Basically the stuff below we be 
-## in the function above  But I am following the example from class
 
-## when cacheinverse is called the name of of the object created by makeCache 
-## above 
-## is passed in as an argument and is assigned to x
+## when cacheSolve is called the object created 
+## by makeCacheMatrix above is passed in as an argument and is 
+## assigned to x
 
 cacheSolve <- function(x, ...) {
 
@@ -91,7 +90,7 @@ cacheSolve <- function(x, ...) {
 ## If the call to x$getinverse() is null 
 ## (i.e this function has never been called)
 ## this next part called to calculate the inverse of the matrix that is going 
-## to be cached. The function get in the makeCache object is called , 
+## to be cached. The function get in the makeCacheMatrix object is called , 
 ## like m1_cashe$get() 
         
         data <- x$get()
